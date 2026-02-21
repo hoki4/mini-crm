82a0bdb4829ddfa4f0d44bd37afddf04ad1918c5
hoki4 <svizygod@gmail.com>
2026-02-21 11:20:00 +0300

    настроил роутинг и добавил useDebounce composable

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       redirect: '/clients',
+    },
+    {
+      path: '/clients',
+      name: 'clients',
+      component: () => import('@/views/ClientList.vue'),
+    },
+    {
+      path: '/clients/new',
+      name: 'client-create',
+      component: () => import('@/views/ClientForm.vue'),
+    },
+    {
+      path: '/clients/:id/edit',
+      name: 'client-edit',
+      component: () => import('@/views/ClientForm.vue'),
     },
   ],
 })
